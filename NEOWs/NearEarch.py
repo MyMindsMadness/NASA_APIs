75d6d1ea59b22c420817d44e984b4bf750abda58
Ian Tony Birchall <iantonybirchall@gmail.com>
2024-01-11 21:32:25 +0000

    clean up of code inc comments

--- NEOWs/NearEarch.py
+++ NEOWs/NearEarch.py
@@ -1,5 +1,6 @@
 #!/home/mccube/PersonalProjects/NASA_API/NASAenv/bin/python3
 
+# Import dependancies #
 import requests 
 import datetime 
 import json
@@ -8,41 +9,29 @@ from pathlib import Path
 import os
 import copy
 
+#Set path to enviroment variables
 my_dotenv_path = Path('NASAenv/env.env')
 load_dotenv(dotenv_path=my_dotenv_path)
 
+# set enviroment variables as code variables
 webexurl = os.getenv("WEBEX_URL")
 webexbot = os.getenv("WEBEX_BEARER")
 webexroomid = os.getenv("WEBEX_ROOM_ID")
-nasaapodurl = os.getenv("NASA_NEO_BASE_URL")
+nasaneourl = os.getenv("NASA_NEO_BASE_URL")
 nasaapi = os.getenv("NASA_API_KEY")
 
-#class NEO(object):
-#    name = ""
-#    size = 0.0
-#    miss = 0.0
-#    approach = 0.0
-#    speed = 0.0
-#    def __init__(self, name, size, miss, approach, speed):
-#        self.name = name
-#        self.size = size
-#        self.miss = miss
-#        self.approach = approach
-#        self.speed = speed
-#    def __str__(self):
-#        return f"{self.name}"
-
 def url_generator():
-    neo_url = os.getenv('NASA_NEO_BASE_URL')
+    #pull the base url
+    neo_url = nasaneourl
+    #generate dates
     start_date = datetime.datetime.today().strftime('%Y-%m-%d')
     end_date = datetime.datetime.now() + datetime.timedelta(days=1)
     end_date = end_date.date()
-    print ("Dates to be used are: ",
-           start_date, "and", 
-           end_date)
     start_date = "start_date="+str(start_date)
     end_date = "end_date="+str(end_date)+"&"
+    #pull personal nasa api key
     api_key = nasaapi
+    #construct url from all above
     url_constructor = neo_url + start_date + end_date + api_key
     return url_constructor
 
@@ -51,25 +40,34 @@ def neo_request(url):
     return json_get
 
 def informant(json_data):
+    #set hazards to 0 
     total_haz = 0
+    #pull total amount of object from the retrieved json data
     totalobjects = json_data["element_count"]
     hazard_objects = []
+    #for loop that looks for all objects with that 'hazardous' value set to true
     for dates in json_data["near_earth_objects"]:
         date = dates
         for object in json_data["near_earth_objects"][date]:
             if object["is_potentially_hazardous_asteroid"] == True:
                 hazard_object = []
+                #add to total amount of hazardous objects
                 total_haz += 1
+                #identify the name of the object
                 object_name = str(object["name"])
+                #identify the data the object will pass
                 object_date = date
+                #calculate miss distance and convert it to GigaMeters(Gm)
                 object_miss = float(object["close_approach_data"][0]["miss_distance"]["kilometers"]) /1000000
+                #the sun is ~1.4Gm in diameter, this will determine how many suns away the object is.
                 size_in_suns = object_miss / 1.4
                 object_miss = str(round(size_in_suns, 2))
+                #appends information to list for passing to card builder
                 hazard_object.append(object_name)
                 hazard_object.append(object_date)
                 hazard_object.append(object_miss)
-
                 hazard_objects.append(hazard_object)
+    #pass list of lists, total objects and count of hazards to card builder
     card_builder(hazard_objects, totalobjects, total_haz)
 
 def card_builder(object_details, tot_objects, tot_hazards):
@@ -80,28 +78,21 @@ def card_builder(object_details, tot_objects, tot_hazards):
     json_data["body"][2]["columns"][1]["items"][0]["text"] = str(tot_hazards)
 
     with open('NEOWs/columnset_builder.json') as g:
+        #take a copy of the data for a template.
         columnset_template= json.load(g)
+    #loop to generate information to be appended to the card.
     for object in object_details:
+        #takes a temp copy of the template
         columnsetdata = copy.deepcopy(columnset_template)
+        #change desired fields
         columnsetdata["columns"][0]["items"][0]["text"] = object[0]
         columnsetdata["columns"][1]["items"][0]["text"] = object[1]
         columnsetdata["columns"][2]["items"][0]["text"] = object[2]
-        print (f"The data to be added is \n{columnsetdata}\n")
+        #append temp data to the data that will be passed to the card as attachment content
         json_data["body"].append(columnsetdata)
-#    for object in object_details:
-#        print (object)
-#        temp_csd["columns"][0]["items"][0]["text"] = object[0]
-#        temp_csd["columns"][1]["items"][0]["text"] = object[1]
-#        temp_csd["columns"][2]["items"][0]["text"] = object[2]
-#        json_data["body"].append(temp_csd)
-#    print(json_data)
-
-    #pretty_print = json.dumps(json_data)
-    #pretty_print = json.dumps(json.loads(pretty_print), indent=2)
-    #print (pretty_print)
-    #for object in object_details:
-
-    url = webexurl   
+    #set webex url
+    url = webexurl
+    #pull all information in that is to be used in the card.
     attachment = {
         "contentType": "application/vnd.microsoft.card.adaptive",
         "content": json_data
